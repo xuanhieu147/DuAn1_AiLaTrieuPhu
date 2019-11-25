f03634aca4573615938e2f9eae5c77e992ce42a5
xuanhieu147 <vietnamvodich1472000@gmail.com>
2019-11-25 14:18:51 +0700

    a

--- app/src/main/java/com/example/duan1/HelpActivity.java
+++ app/src/main/java/com/example/duan1/HelpActivity.java
@@ -2,13 +2,38 @@ package com.example.duan1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
-
+    private Button btnReady;
+    private MediaPlayer mediaPlayer;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        mediaPlayer= MediaPlayer.create(this,R.raw.luatchoi);
+        mediaPlayer.start();
+        initView();
+    }
+
+
+    private void initView() {
+        btnReady = (Button) findViewById(R.id.btnSanSang);
+        btnReady.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                onBackPressed();
+                playSound(R.raw.bg_music);
+            }
+        });
+    }
+
+    public void playSound(int type){
+        mediaPlayer = MediaPlayer.create(this, type);
+        mediaPlayer.start();
     }
 }
--- app/src/main/java/com/example/duan1/MainActivity.java
+++ app/src/main/java/com/example/duan1/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.duan1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnStart;
+    private Button btnHelp;
+    private Button btnExit;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playSound(R.raw.bg_music);
+        initView();
+    }
+
+    private void initView() {
+        btnStart = (Button) findViewById(R.id.btn_batDau);
+        btnHelp = (Button) findViewById(R.id.btn_troGiup);
+        btnExit = (Button) findViewById(R.id.btn_thoat);
+        btnStart.setOnClickListener(this);
+        btnHelp.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_batDau:
+                mediaPlayer.stop();
+                playSound(R.raw.batdauchoi);
+                Intent intent = new Intent(this, StartActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_troGiup:
+                mediaPlayer.stop();
+                Intent intent1 = new Intent(this, HelpActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.btn_thoat:
+                System.exit(0);
+                break;
+        }
+    }
+
+
+    public void playSound(int type) {
+        mediaPlayer = MediaPlayer.create(this, type);
+        mediaPlayer.start();
     }
 }
